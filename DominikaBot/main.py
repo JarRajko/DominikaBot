a911d2b02cd1450c825a3fb9a23ebfc3d6246516
JarRajko <48447054+JarRajko@users.noreply.github.com>
2020-12-03 15:41:52 +0100

    Add files via upload

--- DominikaBot/main.py
+++ DominikaBot/main.py
@@ -1,6 +1,7 @@
 import discord 
 import random
 import os
+import json
 
 from discord.ext import commands, tasks
 from itertools import cycle
@@ -9,13 +10,44 @@ from itertools import cycle
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 bot = commands.Bot(command_prefix = 'Dominika? ', intents = intents)
 status = cycle(['Apex Legends','Genshin Impact','League of Legends'])
-
+players = {}
 
 @bot.event #Event triggered when bot is ready.. :-|
 async def  on_ready():
     pass
     #change_status.start()
     #print("Bot is ready.")
+
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    with open('data.json','r',encoding='utf8') as f:
+            user = json.load(f)
+    
+    try:
+        with open('data.json','w',encoding='utf8') as f:
+            user[str(message.author.id)]['exp'] = user[str(message.author.id)]['exp']+1
+            lvl_start = user[str(message.author.id)]['level']
+            lvl_end = user[str(message.author.id)]['exp'] ** (1.5/4)
+            if lvl_start < lvl_end:
+                user[str(message.author.id)]['level'] = user[str(message.author.id)]['level']+1
+                lvl = user[str(message.author.id)]['level']
+                await message.channel.send(f"{message.author.name} has leveld his bugbot up to {lvl}")
+                json.dump(user,f,sort_keys=True,ensure_ascii=False)
+                return
+            json.dump(user,f,sort_keys=True,indent=4, ensure_ascii=False)
+            print("data updated")
+            
+    except:
+        
+        with open('data.json','w',encoding='utf8') as f:
+            #user = {}
+            user[str(message.author.id)] = {}
+            user[str(message.author.id)]['level'] = 0 
+            user[str(message.author.id)]['exp'] = 0
+            json.dump(user,f,sort_keys=True,indent=4,ensure_ascii=False)
+        print("data inserted")
 
 
 @bot.event #Event triggered when someone joins the server
@@ -88,6 +120,13 @@ def is_check_user(ctx): #Check user
 async def testicek(ctx):
     await ctx.send("Yes masťer.")
 
+@bot.command()
+@commands.has_any_role('Simp')
+async def say(ctx, *, message):
+    await ctx.message.delete()
+    await ctx.send(message)
+
+    
 @clr.error
 async def clr_error(ctx, error):
     if isinstance(error, commands.errors.MissingPermissions):
@@ -103,6 +142,27 @@ for filename in os.listdir('.\cogs')    :
 @tasks.loop(seconds=1800)
 async def change_status():
     await bot.change_presence(activity=discord.Game(next(status)))
+
+@bot.command() # <3 Embed messages
+async def dis(ctx):
+    embed = discord.Embed(
+        title = 'Title',
+        description = 'Description',
+        
+        colour = discord.Colour.red()
+    )
+
+    embed.set_footer(text='Footer lmao')
+    embed.set_image(url='https://cdn.discordapp.com/attachments/783036978127175680/783416344393154610/dominika_portrait_.png')
+    embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/783036978127175680/783416344393154610/dominika_portrait_.png')
+    embed.set_author(name='Big Rajko', icon_url = 'https://cdn.discordapp.com/attachments/783036978127175680/783416344393154610/dominika_portrait_.png')
+    embed.add_field(name='Field name',value='Field Value', inline=False)
+    embed.add_field(name='Field name',value='Field Value', inline=True)
+    embed.add_field(name='Field name',value='Field Value', inline=True)
+
+    await ctx.send(embed=embed)
+
+
 
     
 bot.run('NzgzMDIwNjIwNDUwODg5NzM5.X8Uqxw.O5_xj7SuQBJtR49VFkbynsOW26c')    
