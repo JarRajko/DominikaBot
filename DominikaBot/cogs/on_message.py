7cca3d62c3f9cb04af42a57db423a5cdbdfae483
Radovan Markuš <markus6@stud.uniza.sk>
2021-01-06 12:33:23 +0100

    Druhy commit

---
+++ DominikaBot/cogs/on_message.py
@@ -0,0 +1,40 @@
+import discord
+
+from discord.ext import commands
+
+class On_message(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        channel = message.channel.id
+        #await message.channel.send('message goes here')
+        if (message.content == "Dominika?"):
+            await message.author.send("Chceš s niečim pomôcť? Nah, tu más dostupné príkazy. Keď tak použi pomoc *príkaz *")
+            filename = "./speech/help.txt"
+            f = open(filename,"r",encoding="utf8")
+            content = "".join(f.readlines())
+            f.close()
+            await message.author.send(str(content))
+            channel = message.channel.id
+            await message.channel.send('Pozri si správy ;-)')
+        else:
+            pass
+
+        if (message.content[0] == "/"):
+            filename = "./speech/emotes.txt"
+            f = open(filename,"r",encoding="utf8")
+            content = "".join(f.readlines()).split("\n")
+
+            for i in range (len(content)):
+                if message.content in content[i]:
+                    s = content[i].split("|")
+                    await message.channel.send(s[1])
+                    break
+        
+    
+
+def setup(bot):
+    bot.add_cog(On_message(bot))
